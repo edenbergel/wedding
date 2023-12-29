84adb2399efa5136da0b66c3ae095f3fbbd83519
eden bergel <edenbergel@edens-MacBook-Pro-2.local>
2023-12-29 18:54:33 +0100

    make svg components

---
+++ src/svgs/back.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Back = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="20" viewBox="0 0 48 20" fill="none">
+            <path d="M46.5366 1.98145C43.1706 12.1584 37.7506 17.4914 30.2766 17.9814C22.8026 18.4714 13.1466 14.1244 1.30762 4.94345" stroke="black" strokeWidth="2" strokeLinecap="round"/>
+            <path d="M1.30762 4.94331C2.61962 7.79131 3.41362 9.97031 3.68762 11.4813C3.96362 12.9913 3.96362 15.1713 3.68762 18.0183M1.94062 4.69131C5.01062 5.32931 7.31162 5.61131 8.84562 5.54031C10.3796 5.46831 12.5026 4.97831 15.2156 4.07031" stroke="black" strokeWidth="2" strokeLinecap="round"/>
+        </svg>
+    )
+}
---
+++ src/svgs/index.js
@@ -0,0 +1,5 @@
+export { Logo } from './logo';
+export { CurvedTitle } from './curvedTitle';
+export { Back } from './back';
+export { Link } from './link';
+export { Tampon } from './tampon';
