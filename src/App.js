2eb006e7a1827131cfc652ac9c92517e239c8925
eden bergel <edenbergel@edens-mbp-2.home>
2024-01-06 12:50:55 +0100

    title i18n

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Routing from "./router";
 import { useTranslation } from 'react-i18next';
 
 const App = () => {
-    const { i18n } = useTranslation();
+    const { t, i18n } = useTranslation();
     useEffect(() => {
         const storedLanguage = localStorage.getItem('language');
         if (storedLanguage) {
             i18n.changeLanguage(storedLanguage);
         }
     }, [i18n]);
+
+    document.title = t('dudeName') + ' ' + t('and') + ' ' + t('queenName');
 
     return (
         <Routing />
