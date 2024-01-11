d6b6308bdd2f3c039d57189f47fc0740ced5f112
eden bergel <edenbergel@edens-mbp.home>
2024-01-11 22:52:19 +0100

    fix transition language hover when he

--- src/components/languagePicker/languagePicker.js
+++ src/components/languagePicker/languagePicker.js
@@ -4,13 +4,15 @@ import './languagePicker.scss';
 
 export const LanguagePicker = () => {
     const { t, i18n } = useTranslation();
+    const language = i18n.language;
+    const locale = language.slice(0, 2);
 
     const changeLanguage = (newLanguage) => {
         i18n.changeLanguage(newLanguage);
     };
 
     return (
-        <div className="language_picker">
+        <div className={`language_picker ${locale ? 'language_picker-he' : ''}`}>
             <div onClick={() => changeLanguage('he')}><span>{t('hebrew')}</span></div>
             <div onClick={() => changeLanguage('fr')}><span>{t('french')}</span></div>
         </div>
