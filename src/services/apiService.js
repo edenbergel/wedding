aacda991e2bd0c9d9d256585dfafedb9e50fc7a6
eden bergel <edenbergel@edens-mbp.home>
2024-01-23 23:04:37 +0100

    fix guests maximum entries

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -4,7 +4,7 @@ import { API_BASE_URL } from "../config";
 const apiService = {
     passwordGetFR: () => axios.get(`${API_BASE_URL}/api/passwords/1`),
     passwordGetHE: () => axios.get(`${API_BASE_URL}/api/passwords/2`),
-    guestsGet: () => axios.get(`${API_BASE_URL}/api/guests`),
+    guestsGet: () => axios.get(`${API_BASE_URL}/api/guests?pagination[start]=0&pagination[limit]=350&pagination[withCount]=true`),
     post: (data) => axios.post(`${API_BASE_URL}/api/guests`, { data }),
 };
 
